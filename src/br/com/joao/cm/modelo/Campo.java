8d787544a2edc28235784972791ab2dab9e43dd3
João Henrique De Oliveira <joaohenri293@gmail.com>
2023-07-13 16:02:32 -0300

    Metodos da Classe campo

--- src/br/com/joao/cm/modelo/Campo.java
+++ src/br/com/joao/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.joao.cm.modelo;
 
+import br.com.joao.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class Campo {
         this.COLUNA = coluna;
     }
 
-    public boolean adicionarVizinho(Campo vizinho){
+     public boolean adicionarVizinho(Campo vizinho){
         boolean linhaDiferente = LINHA != vizinho.LINHA;
         boolean colunaDiferente = COLUNA != vizinho.COLUNA;
         boolean diagonal = linhaDiferente && colunaDiferente;
@@ -38,4 +40,86 @@ public class Campo {
             return false;
         }
     }
+
+    public void alternarMarcacao(){
+        if(!aberto){
+            marcado = !marcado;
+        }
+    }
+
+    public boolean abrir(){
+
+        if (!aberto && !marcado){
+            aberto = true;
+
+            if (minado){
+                throw new ExplosaoException();
+            }
+
+            if (vizinhacaSegura()){
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean vizinhacaSegura(){
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+    public void minar(){
+        minado = true;
+    }
+    public boolean isMarcado() {
+        return marcado;
+    }
+
+    public boolean isAberto(){
+        return aberto;
+    }
+
+    public boolean isFechado(){
+        return !isAberto();
+    }
+
+    public int getLINHA() {
+        return LINHA;
+    }
+
+    public int getCOLUNA() {
+        return COLUNA;
+    }
+
+    boolean objetivoAlcancado(){
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+        return desvendado || protegido;
+    }
+
+    long minasNaVizinhanca(){
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar(){
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+
+    public String toString() {
+        if (marcado){
+            return "x";
+        } else if (aberto && minado){
+            return "*";
+        } else if (aberto && minasNaVizinhanca() > 0){
+            return Long.toString(minasNaVizinhanca());
+        } else if (aberto) {
+            return " ";
+        } else {
+            return "?";
+        }
+    }
 }
+
